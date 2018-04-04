8f145e4ccf4410796d56ecd5d0bb14dcc958ec04
Ujwal Battar <ujwal.battar@gmail.com>
2018-04-03 17:59:39 -0700

    Start

---
+++ frontend/lair_bnb.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import configureStore from
+
+document.addEventListener("DOMContentloaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Lair BnB B!</h1>, root);
+});
