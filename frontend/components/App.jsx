dc719e42971b9fe9851c41864eba65e21e64b5a4
Ujwal Battar <ujwal.battar@gmail.com>
2018-04-05 14:55:14 -0700

    Fix Logout Issue

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,6 +6,7 @@ import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
 import NavbarContainer from "./navbar/navbar_container";
 import HomePageContainer from "./home/home_page_container";
+import { AuthRoute, ProtectedRoute } from "../util/route_api_util";
 
 const App = () => (
   <div>
@@ -15,7 +16,7 @@ const App = () => (
     </header>
     <Switch>
       <Route path="/login" component={LoginFormContainer} />
-      <Route path="/signup" component={SignupFormContainer} />
+      <AuthRoute path="/signup" component={SignupFormContainer} />
       <Route exact path="/" component={HomePageContainer} />
     </Switch>
   </div>
--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,26 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// class Navbar extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const loggedOutLinks = () => (
-  <nav className="login-signup">
-    <Link to="/login">Login</Link>
-    &nbsp;&nbsp;or&nbsp;&nbsp;
-    <Link to="/signup">Signup</Link>
-  </nav>
-);
+  render() {
+    const loggedOutLinks = () => (
+      <nav className="login-signup-links">
+        <Link to="/login">Login</Link>
+        &nbsp;&nbsp;or&nbsp;&nbsp;
+        <Link to="/signup">Signup</Link>
+      </nav>
+    );
 
-const loggedInLinks = () => (
-  <nav className="logout">
-    <Link to="/">Logout</Link>
-  </nav>
-);
-
-const Navbar = ({ currentUser }) =>
-  currentUser ? loggedInLinks() : loggedOutLinks();
-// }
+    const loggedInLinks = (currentUser, logout) => (
+      <nav className="logout-links">
+        <button className="header-button" onClick={logout}>
+          Log Out
+        </button>
+      </nav>
+    );
+    return (
+      <span className="nav-bar-links">
+        {this.props.currentUser
+          ? loggedInLinks(this.props.currentUser, this.props.logout)
+          : loggedOutLinks()}
+      </span>
+    );
+  }
+}
 export default Navbar;
+
+// const personalGreeting = (currentUser, logout) => (
+// 	<hgroup className="header-group">
+//     <h2 className="header-name">Hi, {currentUser.username}!</h2>
+//     <button className="header-button" onClick={logout}>Log Out</button>
+// 	</hgroup>
+// );
+
+// <Link to="/login" onClick={logout}>
+//   Log Out
+// </Link>
