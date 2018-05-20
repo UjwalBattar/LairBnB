2cfc2b6293c9c9b641191c12afd096fb0e41709d
Ujwal Battar <ujwal.battar@gmail.com>
2018-05-19 21:24:37 -0700

    search

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -6,7 +6,24 @@ import ListingIndex from "../listings/listing_index";
 class SearchComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchQuery: ""
+    };
+
+    this.update = this.update.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
+
+  update(property) {
+    return e => {
+      this.setState({ [property]: e.target.value }, this.handleSearch);
+    };
+  }
+
+  handleSearch() {
+    this.props.fetchSearchResults(this.state.searchQuery);
+  }
+
   render() {
     return (
       <div className="search-container">
