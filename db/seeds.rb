f1d80f7f647cba9aebe5a938ad42ea6d45f65a77
Ujwal Battar <ujwal.battar@gmail.com>
2018-07-18 22:22:04 -0700

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,14 @@
 Listing.delete_all
 User.delete_all
 
-
+User.create!(
+  first_name: "Clark",
+  last_name: "Kent",
+  username: "Superman",
+  password: "password",
+  email: "superman@mail.com",
+  image: "http://www.monologuedb.com/wp-content/uploads/2011/03/SupermanChristopherReeve.jpg"
+)
 User.create!(
   first_name: "Bruce",
   last_name: "Wayne",
